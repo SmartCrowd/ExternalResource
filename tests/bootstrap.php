259789d667419cfb04b0f410437b0f3d92e93da4
Jack <infinity.jacksparrow@gmail.com>
2015-10-16 18:07:56 +0300

    Real init commit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,31 @@
 <?php
-error_reporting(E_ALL | E_STRICT);
+use Phalcon\DI,
+    Phalcon\DI\FactoryDefault;
 
-require_once 'PHPUnit/TextUI/TestRunner.php';
-require dirname(__DIR__) . '/vendor/autoload.php';
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
+define('ROOT_PATH',  str_replace('/tests', '', __DIR__));
+
+/**
+ * @param string $path
+ * @param bool $isRequire
+ * @return mixed
+ */
+function loadDepends($path, $isRequire = false){
+    try {
+        if ($isRequire)
+            return require_once(ROOT_PATH . $path);
+        else
+            return include_once(ROOT_PATH . $path);
+    } catch (\Exception $e) {
+        return false;
+    }
+}
+
+loadDepends("/vendor/autoload.php");
+
+$config = loadDepends('/app/config/config.php');
+
+include_once(ROOT_PATH . "/app/config/loader.php");
+include_once(ROOT_PATH . "/app/config/services.php");
