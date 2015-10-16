3d48a29a52417828e749c3bb5ea9f24d5c43ad46
Jack <infinity.jacksparrow@gmail.com>
2015-10-16 17:42:25 +0300

    Real init commit

---
+++ tests/ExternalResourceTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace assets;
+
+class AssetsManagerTest extends \PHPUnit_Framework_TestCase {
+
+    public function testGetFilesFromDir()
+    {
+         $this->assertNotEmpty('1');
+
+    }
+
+}
+ 
